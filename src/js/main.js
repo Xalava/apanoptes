e618d4e0c0ec08cc5e277547bf430ed36b8f5187
Xavier Lavayssière <xavier.lavayssiere@gmail.com>
2018-11-03 15:59:12 +0100

    Displays Basic Info

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,47 @@
-var web3 = new Web3(Web3.givenProvider || "https://localhost:8545");
-const RPCServer = '127.0.0.1'
-const LogsServer = new WebSocket("ws://SERVER.com/socketserver", "protocolOne")
+const RPCServer = "http://127.0.0.1:8545"
+const WSServer = "";
 
-web3.eth.net.getId()
-.then(console.log);
+$(function() {
+  var web3 = new Web3(Web3.givenProvider || RPCServer );
+  console.log("WEB3 is here ! : ",web3)
+  const logs = new WebSocket("ws://SERVER.com/socketserver", "protocolOne")
+  
+  web3.eth.net.getId()
+  .then( 
+    (res)=> {
+      $( "#netid" ).html( res );
+    }
+  );
+  
+  web3.eth.getBlockNumber()
+  .then(
+    (res)=> {
+      $( "#lastblock" ).html( res );
+    }
+  );
+
+  if (WSServer) {
+    var socket = io(WSServer);
+      socket.emit('tail', {test: "1233"});
+      socket.on('newLine', function(msg){
+        $('#messages').append($('<li>').text(msg.line));
+    });
+  } else {
+    console.log("No logs Server")
+  }
+
+  
+});  
 
 
-web3.eth.getBlockNumber()
-.then(console.log);
+// var socket = io();
+//   socket.emit('tail', {test: "1233"});
+//   socket.on('newLine', function(msg){
+//     $('#messages').append($('<li>').text(msg.line));
+// });
 
 
-var socket = io();
-  socket.emit('tail', {test: "1233"});
-  socket.on('newLine', function(msg){
-    $('#messages').append($('<li>').text(msg.line));
-});
-
-
-$.get( "ajax/test.html", function( data ) {
-  $( ".result" ).html( data );
-  alert( "Load was performed." );
-});
+// $.get( "ajax/test.html", function( data ) {
+//   $( ".result" ).html( data );
+//   alert( "Load was performed." );
+// });
